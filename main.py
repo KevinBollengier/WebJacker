4677b3016a16e454e17ef46d681c2fe2e44c7e14
Kevin <kevin.bollengier@student.howest.be>
2018-06-14 09:10:15 +0200

    Refactored into main function

--- main.py
+++ main.py
@@ -6,45 +6,53 @@ import functions
 # TODO refactor static file name
 # TODO make program more verbose
 # TODO clean up code
-try:
-    report_output = []
 
-    url = input("Enter website to test:")
 
-    r = requests.get(url="http://" + url)
-    report_output.append('# W3bJ4ck3r Report - ' + url)
-    report_output.append('## General Information')
-    report_output.append('\tURL : ' + url)
-    report_output.append('\tStatus Code : ' + str(r.status_code))
+def main():
     try:
-        sr = requests.get(url="https://" + url, verify=True)
-        report_output.append("\tHTTPS : OK")
-        if 'strict-transport-security' in sr.headers:
-            report_output.append('\tHSTS : Yes')
-        else:
-            report_output.append("\tHSTS : No")
-    except requests.exceptions.SSLError:
-        report_output.append("\tHTTPS : Errors with HTTPS certificate.")
-    report_output.append('## Request Headers')
-    for header in r.request.headers:
-        report_output.append("\t" + header + " : " + r.request.headers[header])
-    report_output.append('## Response Headers')
-    for header in r.headers:
-        report_output.append("\t" + header + " : " + r.headers[header])
-    report_output.append('## DNS Dump ')
-    report_output.append(functions.dns_dump(url, 'CNAME'))
-    report_output.append(functions.dns_dump(url, 'SOA'))
-    report_output.append(functions.dns_dump(url, 'A'))
-    report_output.append(functions.dns_dump(url, 'NS'))
-    report_output.append(functions.dns_dump(url, 'MX'))
-    report_output.append(functions.dns_dump(url, 'HINFO'))
-    report_output.append('## Port Scan')
-    open_ports = functions.simple_portscan(url)
-    for line in open_ports:
-        report_output.append("\t"+line+"\n")
-    report_output.append('********')
-    functions.print_to_md_file(report_output)
-except KeyboardInterrupt:
-    print("[*] User requested an interrupt")
-    print("[*] Shutting down")
-    sys.exit(1)
+        report_output = []
+
+        url = input("Enter website to test:")
+
+        r = requests.get(url="http://" + url)
+        report_output.append('# W3bJ4ck3r Report - ' + url)
+        report_output.append('## General Information')
+        report_output.append('\tURL : ' + url)
+        report_output.append('\tStatus Code : ' + str(r.status_code))
+        try:
+            sr = requests.get(url="https://" + url, verify=True)
+            report_output.append("\tHTTPS : OK")
+            if 'strict-transport-security' in sr.headers:
+                report_output.append('\tHSTS : Yes')
+            else:
+                report_output.append("\tHSTS : No")
+        except requests.exceptions.SSLError:
+            report_output.append("\tHTTPS : Errors with HTTPS certificate.")
+        report_output.append('## Request Headers')
+        for header in r.request.headers:
+            report_output.append("\t" + header + " : " + r.request.headers[header])
+        report_output.append('## Response Headers')
+        for header in r.headers:
+            report_output.append("\t" + header + " : " + r.headers[header])
+        report_output.append('## DNS Dump ')
+        report_output.append(functions.dns_dump(url, 'CNAME'))
+        report_output.append(functions.dns_dump(url, 'SOA'))
+        report_output.append(functions.dns_dump(url, 'A'))
+        report_output.append(functions.dns_dump(url, 'NS'))
+        report_output.append(functions.dns_dump(url, 'MX'))
+        report_output.append(functions.dns_dump(url, 'HINFO'))
+        report_output.append('## Port Scan')
+        open_ports = functions.simple_portscan(url)
+        for line in open_ports:
+            report_output.append("\t" + line + "\n")
+        report_output.append('********')
+        functions.print_to_md_file(report_output)
+    except KeyboardInterrupt:
+        print("[*] User requested an interrupt")
+        print("[*] Shutting down")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
+
